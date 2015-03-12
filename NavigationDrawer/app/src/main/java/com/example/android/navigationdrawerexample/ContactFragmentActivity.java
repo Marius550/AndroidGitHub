abf9cb0f94aa197cb715583b7a0b3d44b4dcbae3
Marius550 <mariuspilgrim@mariusmacbook.speedport_w_723v_1_36_000>
2015-03-12 19:55:48 +0100

    14. Commit
    1. Completed code
    2. Prepare contact menu with email contact

--- NavigationDrawer/app/src/main/java/com/example/android/navigationdrawerexample/ContactFragmentActivity.java
+++ NavigationDrawer/app/src/main/java/com/example/android/navigationdrawerexample/ContactFragmentActivity.java
@@ -1,26 +1,25 @@
 package com.example.android.navigationdrawerexample;
 
 import android.app.Activity;
-import android.app.SearchManager;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
 
 /**
  * Created by mariuspilgrim on 12/03/15.
  */
 public class ContactFragmentActivity extends Activity {
 
-    //public final static String EXTRA_MESSAGE_NAME = "com.example.android.navigationdrawerexample.MESSAGE_NAME";
-    //public final static String EXTRA_MESSAGE_EMAIL = "com.example.android.navigationdrawerexample.MESSAGE_EMAIL";
+    /*
+    public final static String EXTRA_MESSAGE_FIRST_NAME = "com.example.android.navigationdrawerexample.MESSAGE_FIRST_NAME";
+    public final static String EXTRA_MESSAGE_LAST_NAME = "com.example.android.navigationdrawerexample.MESSAGE_LAST_NAME";
+    public final static String EXTRA_MESSAGE_EMAIL = "com.example.android.navigationdrawerexample.MESSAGE_EMAIL";
+    */
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,59 +35,11 @@ public class ContactFragmentActivity extends Activity {
         return super.onCreateOptionsMenu(menu);
     }
 
-    /* Called whenever we call invalidateOptionsMenu() */
     /*
     @Override
-    public boolean onPrepareOptionsMenu(Menu menu) {
-        // If the nav drawer is open, hide action items related to the content view
-        boolean drawerOpen = mDrawerLayout.isDrawerOpen(mDrawerList);
-        menu.findItem(R.id.action_websearch).setVisible(!drawerOpen);
-        return super.onPrepareOptionsMenu(menu);
-    }
-    */
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-    /*
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // The action bar home/up action should open or close the drawer.
-        // ActionBarDrawerToggle will take care of this.
-        if (mDrawerToggle.onOptionsItemSelected(item)) {
-            return true;
-        }
-        // Handle action buttons
-        switch(item.getItemId()) {
-            case R.id.action_websearch:
-                // create intent to perform web search for this planet
-                Intent intent = new Intent(Intent.ACTION_WEB_SEARCH);
-                intent.putExtra(SearchManager.QUERY, getActionBar().getTitle());
-                // catch event that there's no activity to handle intent
-                if (intent.resolveActivity(getPackageManager()) != null) {
-                    startActivity(intent);
-                } else {
-                    Toast.makeText(this, R.string.app_not_available, Toast.LENGTH_LONG).show();
-                }
-                return true;
-            case R.id.action_settings:
-                openAndroidSettings();
-                return true;
-            case R.id.action_browser:
-                openAndroidBrowser();
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
-        }
-    }
-    */
-
-
-
-    /*
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-
-        View view = inflater.inflate(R.layout.fragment_contact,
-                container, false);
+        View view = inflater.inflate(R.layout.fragment_contact, container, false);
         Button button = (Button) view.findViewById(R.id.btn_setDefaultInputValues);
         button.setOnClickListener(new View.OnClickListener()
         {
@@ -96,50 +47,28 @@ public class ContactFragmentActivity extends Activity {
             public void onClick(View v)
             {
                 // do something
-                setDefaultInputValues(v);
             }
         });
         return view;
     }
-    */
 
     /**
-     * Called when the user clicks the Send button
+     * Called when the user clicks the onClick="setDefaultInputValues" button
      * @param view
      */
     /*
-    public void sendMessage(View view) {
-        //Do something in response to button
-        Intent intent = new Intent (this, ContactResultFragmentActivity.class);
+    public void setDefaultInputValuesNotMain(View view) {
 
-        EditText editTextName = (EditText) findViewById(R.id.edit_name);
+        System.out.print("setDefaultInputValuesNotMain");
+
+        EditText editTextFirstName = (EditText) findViewById(R.id.edit_first_name);
+        EditText editTextLastName = (EditText) findViewById(R.id.edit_last_name);
         EditText editTextEmail = (EditText) findViewById(R.id.edit_email);
 
-        String messageName = editTextName.getText().toString();
-        String messageEmail = editTextEmail.getText().toString();
-
-        intent.putExtra(EXTRA_MESSAGE_NAME, messageName);
-        intent.putExtra(EXTRA_MESSAGE_EMAIL, messageEmail);
-        startActivity(intent);
+        editTextFirstName.setText("Max");
+        editTextLastName.setText("Mustermann");
+        editTextEmail.setText("max.mustermann@gmail.de");
     }
     */
-
-    /**
-     * Set input elements to default values for Elena
-     * @param view
-     */
-
-    /*
-    public void setDefaultInputValues(View view) {
-        System.out.println("setDefaultInputValues view: " + view.toString());
-
-        EditText editTextName = (EditText) findViewById(R.id.edit_name);
-        EditText editTextEmail = (EditText) findViewById(R.id.edit_email);
-
-        editTextName.setText("Elena");
-        editTextEmail.setText("elena.enbrecht@gmail.de");
-    }
-    */
-
 
 }
